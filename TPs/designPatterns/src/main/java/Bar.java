ec88c34e67301fb70d1ab8fde47edc552e4cba50
catarina03 <up201806610@fe.up.pt>
2020-03-26 19:55:52 +0000

    3. Observer Pattern

---
+++ TPs/designPatterns/src/main/java/Bar.java
@@ -0,0 +1,28 @@
+import java.util.ArrayList;
+import java.util.List;
+
+public abstract class Bar {
+    private List<BarObserver> observers;
+
+    public Bar() {
+        observers = new ArrayList<BarObserver>();
+    }
+
+    public abstract boolean isHappyHour();
+    public abstract void startHappyHour();
+    public abstract void endHappyHour();
+
+    public void addObserver(BarObserver observer) {
+        observers.add(observer);
+    }
+
+    public void removeObserver(BarObserver observer) {
+        observers.remove(observer);
+    }
+
+    public void notifyObservers() {
+        for (BarObserver observer : observers)
+            if (isHappyHour()) observer.happyHourStarted(this);
+            else observer.happyHourEnded(this);
+    }
+}
